aa30fa14e2cb2861c8f4522d243c7c728796671a
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-04-28 17:30:26 +0200

    styling van baisc elements

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.get("/", async (request, response) => {
   }
 });
 
+
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), function () {
   console.log(`Application started on http://localhost:${app.get("port")}`);
